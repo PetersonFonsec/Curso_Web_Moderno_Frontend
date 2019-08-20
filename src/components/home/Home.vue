2d32bc2fedf279f3dcea755d8232fc6fc559f0de
Peterson Fonseca <petersonsimiao1@gmail.com>
2019-08-19 23:54:49 -0300

    add Components PageTitle

---
+++ src/components/home/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <PageTitle main="Dashboard" icon="fa fa-home" sub="Base de Conhecimento" />
+</template>
+
+<script>
+import PageTitle from '@/components/template/PageTitle'
+export default {
+    name: 'Home',
+    components: { PageTitle }
+}
+</script>
+
+<style>
+
+</style>
