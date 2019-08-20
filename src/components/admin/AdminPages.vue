cffa3f5fc1fc5a0284abf7aca930832d16886cda
Peterson Fonseca <petersonsimiao1@gmail.com>
2019-08-20 00:06:38 -0300

    add routes '/' and '/admin'

---
+++ src/components/admin/AdminPages.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="admin-pages">
+      <PageTitle main="Administrção do sistema" icon="fa fa-cogs" sub="Cadastro & cia" />
+  </div>
+</template>
+
+<script>
+import PageTitle from '@/components/template/PageTitle'
+export default {
+    name: 'AdminPages',
+    components: { PageTitle }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/config/router.js
@@ -0,0 +1,22 @@
+import  Vue from 'vue'
+import Router from 'vue-router'
+import Home from '@/components/home/Home'
+import AdminPages from '@/components/admin/AdminPages'
+
+Vue.use(Router)
+
+export default new Router({
+    mode: 'history',
+    routes:  [
+        {
+            name: 'home',
+            path: '/',
+            component: Home
+        },
+        {
+            name: 'adminPages',
+            path: '/admin',
+            component: AdminPages
+        }
+    ]
+})
